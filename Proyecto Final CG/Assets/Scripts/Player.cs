71acf26bb7ce4c9a4e0f5c1a60c109c50d621335
juanmayorquin <127550805+juanmayorquin@users.noreply.github.com>
2023-05-10 16:36:43 -0500

    inicio

---
+++ Proyecto Final CG/Assets/Scripts/Player.cs
@@ -0,0 +1,125 @@
+using Cinemachine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private CharacterController characterController;
+    [SerializeField] private float speed, runningSpeed, gravity;
+    [SerializeField] private Transform cam;
+    [SerializeField] private GameObject visual;
+    [SerializeField] private CinemachineVirtualCamera firstPersonCam;
+    [SerializeField] private CinemachineFreeLook thirdPersonCam;
+    bool aiming = false;
+    Animator animator;
+    float rotationSpeed;
+
+    private void Start()
+    {        
+        characterController = GetComponent<CharacterController>();
+        animator = visual.GetComponent<Animator>();
+
+        SwitchThirdPersonCam();
+    }
+
+    private void Update()
+    {
+        
+        if(Input.GetKey(KeyCode.Mouse1))
+        {
+            Aim();
+        }
+        else
+        {
+            CamSwitcher.SwitchCamera(thirdPersonCam);
+        }
+
+        if(CamSwitcher.activeCamera == firstPersonCam)
+        {
+            MoverFristPerson();
+        }
+        else
+        {
+            MoverThirdPerson();
+        }
+    }
+
+    void MoverThirdPerson()
+    {
+        float movementSpeed;
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            movementSpeed = runningSpeed;
+        }
+        else
+        {
+            movementSpeed = speed;
+        }
+
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+
+        Vector3 direccion = new Vector3(horizontal, 0, vertical).normalized;
+
+        if (direccion.magnitude > 0)
+        {
+            float targetAngle = Mathf.Atan2(direccion.x, direccion.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref rotationSpeed, 0.1f);
+
+            Vector3 moveDir = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
+
+            if (movementSpeed == speed)
+            {
+                animator.SetBool("walk", true);
+                animator.SetBool("run", false);
+            }
+
+            if (movementSpeed == runningSpeed)
+            {
+                animator.SetBool("walk", false);
+                animator.SetBool("run", true);
+            }
+
+            transform.rotation = Quaternion.Euler(0, angle, 0);
+            characterController.Move(moveDir.normalized * movementSpeed * Time.deltaTime);
+        }
+        else
+        {
+            animator.SetBool("walk", false);
+            animator.SetBool("run", false);
+        }
+    }
+
+    void MoverFristPerson()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        transform.localEulerAngles = new Vector3(0, cam.localEulerAngles.y, 0);
+        Vector3 direccion = ((transform.forward * vertical) + (transform.right * horizontal)).normalized;
+        characterController.Move(direccion * speed * 0.5f * Time.deltaTime);
+    }
+
+    void Aim()
+    {
+        SwitchFirstPersonCam();
+        aiming = true;
+    }
+
+    void SwitchFirstPersonCam()
+    {
+        firstPersonCam.Priority = 10;
+        thirdPersonCam.Priority = 0;
+    }
+
+    void SwitchThirdPersonCam()
+    {
+        thirdPersonCam.Priority = 10;
+        firstPersonCam.Priority = 0;
+    }
+}

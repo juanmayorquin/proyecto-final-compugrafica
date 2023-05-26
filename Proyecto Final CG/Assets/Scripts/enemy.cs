294cbd8508834afa32e82445f45de1a2d34f8044
jGarzon13 <juliangarzontoro@gmail.com>
2023-05-25 20:15:32 -0500

    ME PEGA
    JAJAJ MASOQUISTA

--- Proyecto Final CG/Assets/Scripts/enemy.cs
+++ Proyecto Final CG/Assets/Scripts/enemy.cs
@@ -9,6 +9,7 @@ public class enemy : MonoBehaviour
     public Animator ani;
     public Quaternion angulo;
     public float grado;
+    public bool atak;
 
     public GameObject Target;
     // Start is called before the first frame update
@@ -23,48 +24,71 @@ public class enemy : MonoBehaviour
     public void Comportamiento_Enemy()
     {
 
-        if(Vector3.Distance(transform.position,Target.transform.position)>5)
+        if (Vector3.Distance(transform.position, Target.transform.position) > 10)
         {
             ani.SetBool("run", false);
-        cronometro += 1 * Time.deltaTime;
-        if(cronometro >=4 )
-        {
-            rutina = Random.Range(0, 2);
-            cronometro = 0;
-        }
-        switch(rutina)
-        {
-            case 0:
-                ani.SetBool("walk", false);
-                break;
-            case 1:
-                grado = Random.Range(0, 360);
-                angulo = Quaternion.Euler(0, grado, 0);
-                rutina++;
-                break;
-            case 2:
-                transform.rotation = Quaternion.RotateTowards(transform.rotation, angulo, 0.5f);
-                transform.Translate(Vector3.forward * 1 * Time.deltaTime);
-                ani.SetBool("walk", true);
+            cronometro += 1 * Time.deltaTime;
+            if (cronometro >= 4)
+            {
+                rutina = Random.Range(0, 2);
+                cronometro = 0;
+            }
+            switch (rutina)
+            {
+                case 0:
+                    ani.SetBool("walk", false);
+                    break;
+                case 1:
+                    grado = Random.Range(0, 360);
+                    angulo = Quaternion.Euler(0, grado, 0);
+                    rutina++;
+                    break;
+                case 2:
+                    transform.rotation = Quaternion.RotateTowards(transform.rotation, angulo, 0.5f);
+                    transform.Translate(Vector3.forward * 1 * Time.deltaTime);
+                    ani.SetBool("walk", true);
 
 
 
-                break;
+                    break;
 
-    
-        }
+
+            }
         }
         else
         {
-            var lookPos = Target.transform.position - transform.position;
-            lookPos.y = 0;
-            var rotation = Quaternion.LookRotation(lookPos);
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, 2);
-            ani.SetBool("walk", false);
+            if (Vector3.Distance(transform.position, Target.transform.position) > 2 && !atak)
+            {
 
-            ani.SetBool("run", true);
-            transform.Translate(Vector3.forward * 2 * Time.deltaTime);
+
+                var lookPos = Target.transform.position - transform.position;
+                lookPos.y = 0;
+                var rotation = Quaternion.LookRotation(lookPos);
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, 2);
+                ani.SetBool("walk", false);
+
+                ani.SetBool("run", true);
+                transform.Translate(Vector3.forward * 2 * Time.deltaTime);
+                ani.SetBool("attack", false);
+            }
+            else
+            {
+                Debug.Log("pegue");
+                ani.SetBool("walk", false);
+                ani.SetBool("run", false);
+
+                ani.SetBool("attack", true);
+                atak = true;
+
+            }
         }
+    }
+
+    public void finalAni()
+    {
+        ani.SetBool("attack", false);
+        atak = false;
+         
     }
     // Update is called once per frame
     void Update()

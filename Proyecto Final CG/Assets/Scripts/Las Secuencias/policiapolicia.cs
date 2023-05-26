b0cce33dc5cdbaf4b02ec74548785fbe78fb397d
Rocazzz <camiloakarck@gmail.com>
2023-05-26 12:12:30 -0500

    cositis

--- Proyecto Final CG/Assets/Scripts/Las Secuencias/policiapolicia.cs
+++ Proyecto Final CG/Assets/Scripts/Las Secuencias/policiapolicia.cs
@@ -1,18 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class policiapolicia : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   public void EscenaJuego()
+   {
+     SceneManager.LoadScene("CinematicaInicial");
+   }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   public void Salir()
+   {
+    Application.Quit();
+   }
 }
